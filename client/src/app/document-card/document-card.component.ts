67c5124990f58fb4ed94383c96b470f2bb943428
Chakib Afraoucene <fc_afraoucene@esi.dz>
2019-07-30 21:00:51 +0100

    material components

---
+++ client/src/app/document-card/document-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'document-card',
+  templateUrl: './document-card.component.html',
+  styleUrls: ['./document-card.component.css']
+})
+export class DocumentCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
