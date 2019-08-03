e6b0f1e416dced7977275675684732e4a70a0a0a
Chakib Afraoucene <fc_afraoucene@esi.dz>
2019-08-03 16:09:17 +0100

    demande permis

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { DocumentCardComponent } from './document-card/document-card.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { HomeComponent } from './home/home.component';
+import { DemandeComponent } from './demande/demande.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SidenavComponent,
-    DocumentCardComponent
+    DocumentCardComponent,
+    DocumentsComponent,
+    HomeComponent,
+    DemandeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path:'documents',
+        component:DocumentsComponent
+      },
+      {
+        path:'',
+        component:HomeComponent
+      },
+      {
+        path:'demande',
+        component:DemandeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
