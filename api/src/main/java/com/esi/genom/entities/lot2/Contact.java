c32f0d3e30352e90c43a1ce566eb2e4019bb7489
Dases95 <dases@protonmail.com>
2019-08-05 12:25:20 +0200

    create users entities and lot2 entities

--- api/src/main/java/com/esi/genom/entities/lot2/Contact.java
+++ api/src/main/java/com/esi/genom/entities/lot2/Contact.java
@@ -4,33 +4,51 @@
  */
 package com.esi.genom.entities.lot2;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.esi.genom.entities.users.User;
+
 @Entity
 @Table(name = "CONTACT")
-public class Contact {
-	@Column
+public class Contact implements Serializable{
+	private static final long serialVersionUID = -1798070786993154676L;
+	@Column(name = "contact_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private String id;
-	@Column
+	@Column(name = "contact_email")
 	private String email;
-	@Column
+	@Column(name = "contact_tel")
 	private String telephone;
-	@Column
+	@Column(name = "contact_adresse")
 	private String adresse;
-	@Column
+	@Column(name = "contact_date_ajout")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date date_ajout;
+	@Column(name = "contact_valide")
+	@NotNull
+	private Boolean valide;
+	@OneToOne
+	@JoinColumn(name="USER_ID")
+	private User creator;
+	
+	
 	public String getId() {
 		return id;
 	}
@@ -60,6 +78,12 @@ public class Contact {
 	}
 	public void setDate_ajout(Date date_ajout) {
 		this.date_ajout = date_ajout;
+	}
+	public Boolean getValide() {
+		return valide;
+	}
+	public void setValide(Boolean valide) {
+		this.valide = valide;
 	}
 
 }
---
+++ api/src/main/java/com/esi/genom/entities/lot2/Lien.java
@@ -0,0 +1,95 @@
+package com.esi.genom.entities.lot2;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.esi.genom.entities.users.User;
+
+@Entity
+@Table(name = "LIENT")
+public class Lien implements Serializable {
+	private static final long serialVersionUID = -1234563372846798580L;
+	
+	
+	@Column(name = "lien_id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	
+	@Column(name = "lien_titre", length = 200)
+	@NotNull
+    @Size(min = 4, max = 200)
+	private String titre ;
+	@Column(name = "lien_value", length = 200,unique = true)
+	@NotNull
+    private String lien;
+	
+	@Column(name = "lien_date_ajout")
+	@NotNull
+	private Date date_ajout;
+	
+	@Column(name = "lien_valide")
+	@NotNull
+	private Boolean valide;
+	@OneToOne
+	@JoinColumn(name="USER_ID")
+	private User creator;
+	
+	
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getLien() {
+		return lien;
+	}
+
+	public void setLien(String lien) {
+		this.lien = lien;
+	}
+
+	public Date getDate_ajout() {
+		return date_ajout;
+	}
+
+	public void setDate_ajout(Date date_ajout) {
+		this.date_ajout = date_ajout;
+	}
+
+	public Boolean getValide() {
+		return valide;
+	}
+
+	public void setValide(Boolean valide) {
+		this.valide = valide;
+	}
+	
+	
+
+}
