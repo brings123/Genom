1c139bc6de7c4a70795950b5b50de66d72e698ca
bachirLadj <fb_ladjouzi@esi.dz>
2019-08-26 03:38:05 +0200

    first commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { PubnavComponent } from './pubnav/pubnav.component';
 import { DocumentCardComponent } from './document-card/document-card.component';
 import { DocumentsComponent } from './documents/documents.component';
 
@@ -12,16 +13,43 @@ import { HomeComponent } from './home/home.component';
 import { DemandeComponent } from './demande/demande.component';
 import { AdminDocumentComponent } from './admin/admin-document/admin-document.component';
 
+import { NewsComponent } from './news/news.component';
+import { AddNewsComponent } from './news/addnews/addnews.component';
+import { InfoFicheComponent } from './infofiche/infofiche.component';
+import { AddFicheComponent } from './infofiche/addfiche/addfiche.component';
+import { EventComponent } from './event/event.component';
+import { AddEventComponent } from './event/addevent/addevent.component';
+import { VideoComponent } from './video/video.component';
+import { AddVideoComponent } from './video/addvideo/addvideo.component';
+import { LinkComponent } from './link/link.component';
+import { AddLinkComponent } from './link/addlink/addlink.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewContactComponent } from './contact/newContact/newcontact.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SidenavComponent,
+    PubnavComponent, 
     DocumentCardComponent,
     DocumentsComponent,
     HomeComponent,
     DemandeComponent,
-    AdminDocumentComponent
+    AdminDocumentComponent,
+
+    NewsComponent,
+    AddNewsComponent,
+    InfoFicheComponent,
+    AddFicheComponent,
+    EventComponent,
+    AddEventComponent,
+    VideoComponent,
+    AddVideoComponent,
+    LinkComponent,
+    AddLinkComponent,
+    ContactComponent,
+    NewContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,7 +70,56 @@ import { AdminDocumentComponent } from './admin/admin-document/admin-document.co
       {
         path:'admin/documents',
         component:AdminDocumentComponent
-      }
+      },
+      {
+        path: 'news',
+        component: NewsComponent
+      },
+      {
+        path: 'addnews',
+        component: AddNewsComponent
+      },
+      {
+        path: 'infofiche',
+        component: InfoFicheComponent
+      },
+      {
+        path: 'addfiche',
+        component: AddFicheComponent
+      },
+      {
+        path: 'events',
+        component: EventComponent
+      },
+      {
+        path: 'addevent',
+        component: AddEventComponent
+      },
+      {
+        path: 'videos',
+        component: VideoComponent
+      },
+      {
+        path: 'addvideo',
+        component: AddVideoComponent
+      },
+      {
+        path: 'links',
+        component: LinkComponent
+      },
+      {
+        path: 'addlink',
+        component: AddLinkComponent
+      },
+      {
+        path: 'contacts',
+        component: ContactComponent
+      },
+      {
+        path: 'newcontact',
+        component: NewContactComponent
+      },
+
     ])
   ],
   providers: [],
